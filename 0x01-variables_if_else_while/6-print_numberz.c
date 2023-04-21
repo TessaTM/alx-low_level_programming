265506098d58a273201e07a41c2f3d083c646ec0
TessaTM <t.a.a.a.mensah@gmail.com>
2023-04-21 15:43:10 -0700

    deleted and redid for betty error

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,14 @@
 #include <stdio.h>
 /**
- * main - putchar prints 0 to 9
+ * main - print single digit base ten
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int number;
-	
+
 	for (number = 0; number < 10; number++)
-	{
-	       putchar((number % 10) + '0');
-	}
+		putchar((number % 10) + '0');
 	putchar('\n');
 	return (0);
 }
